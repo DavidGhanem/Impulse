56440c59a694ed0cd3acac2ee3c2b8ef1b69cbb4
Zibo <davidghanem0@gmail.com>
2026-05-30 15:09:39 +0300

    add sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-})
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://impulsegrowth.online",
+    }),
+  ],
+  base: "/",
+});
